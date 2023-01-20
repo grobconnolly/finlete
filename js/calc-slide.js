4aaf60c9569f35b7e752cde5f71763b510be9d79
Aftab Hussain <aftab@skillforte.co>
2023-01-21 00:58:25 +0500

    Added "M" to the slider tip

--- js/calc-slide.js
+++ js/calc-slide.js
@@ -1,136 +1,131 @@
-var incrementWidthM = 100 / (300);
+var incrementWidthM = 100 / 300;
 var SharePrice = "9";
 var ratio = "0.05";
 var ratiobased = "100000";
 var $1M = 10000000;
 let value;
 
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#SharePrice-label").text("$" + SharePrice);
 
+  const ch = $("span.ui-slider-label");
 
-    $('#SharePrice-label').text("$" + SharePrice);
+  $("span.ui-slider-label").click(function (e) {
+    alert("called");
+  });
+  // console.log(ch);
 
-    const ch = $("span.ui-slider-label");
+  $("#Investment").on("input", function () {
+    update(1);
+  });
 
-    $("span.ui-slider-label").click(function(e) {
-        alert("called");
-    });
-    // console.log(ch);
-
-    $("#Investment").on("input", function() {
-        update(1);
+  $("#price-range")
+    .slider({
+      animate: true,
+      value: 25,
+      min: 0,
+      max: 300,
+      // step:30,
+      //step: 7,
+      slide: function (event, ui) {
+        update(1, ui.value); //changed
+      },
     })
-
-    $("#price-range").slider({
-        animate: true,
-        value: 25,
-        min: 0,
-        max: 300,
-        // step:30,
-        //step: 7,
-        slide: function(event, ui) {
-            update(1, ui.value); //changed
-        }
-    }).slider("pips", {
-
-        rest: "label",
-        step: 50,
-        prefix: "$",
-        suffix: "M",
-    }).slider("float", {
-        slide: function(event, ui) {
-            update(1, ui.value); //changed
-        }
+    .slider("pips", {
+      rest: "label",
+      step: 50,
+      prefix: "$",
+      suffix: "M",
+    })
+    .slider("float", {
+      suffix: "M",
+      slide: function (event, ui) {
+        update(1, ui.value); //changed
+      },
     });
-    $("#price-range").find("span .ui-slider-label").click(function(e) {
-        update(1, $(this).attr("data-value"));
+  $("#price-range")
+    .find("span .ui-slider-label")
+    .click(function (e) {
+      update(1, $(this).attr("data-value"));
     });
-    //Added, set initial value.
-    $("#Earnings").val(0);
-    $("#Earnings-label").text(0);
+  //Added, set initial value.
+  $("#Earnings").val(0);
+  $("#Earnings-label").text(0);
 
-    update();
-
-})
+  update();
+});
 
 //changed. now with parameter
 function update(slider, val) {
-    if (val == undefined && value == undefined) {
-        val = 25;
-    } else if (value == undefined || val != undefined) {
-        value = val;
-    } else if (val == undefined) {
-        val = value
-    }
-    $(".filler").width(val * incrementWidthM + "%");
-    //changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
-    var $PotentialCareerEarnings = slider == 1 ? val : $("#Earnings").val();
-    var $total = $PotentialCareerEarnings * $1M;
+  if (val == undefined && value == undefined) {
+    val = 25;
+  } else if (value == undefined || val != undefined) {
+    value = val;
+  } else if (val == undefined) {
+    val = value;
+  }
+  $(".filler").width(val * incrementWidthM + "%");
+  //changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
+  var $PotentialCareerEarnings = slider == 1 ? val : $("#Earnings").val();
+  var $total = $PotentialCareerEarnings * $1M;
 
-    $(".tooltipslider").html("<i></i> " + $PotentialCareerEarnings + "M");
+  $(".tooltipslider").html("<i></i> " + $PotentialCareerEarnings + "M");
 
+  $("#Earnings-label").text("$" + $total);
+  $("#Earnings").val($total);
 
-    $("#Earnings-label").text("$" + $total);
-    $("#Earnings").val($total);
+  var Investment = $("#Investment").val().replace(/,/g, "");
+  var $PotentialCareerEarnings = $("#Earnings").val().replace(/,/g, "");
 
+  if ($("#Investment").is(":visible")) {
+    $("#Investment").addClass("error");
+    $("#nextstep").addClass("error");
+    if ($("#Investment").val().length == 0) return false;
+  }
 
-    var Investment = $('#Investment').val().replace(/,/g, "");
-    var $PotentialCareerEarnings = $('#Earnings').val().replace(/,/g, "");
+  $("#nextstep").removeClass("error");
+  $("#Investment").removeClass("error");
 
-    if ($('#Investment').is(":visible")) {
-        $('#Investment').addClass("error");
-        $('#nextstep').addClass('error');
-        if ($('#Investment').val().length == 0) return false;
-    }
+  var resultratio = ratio / ratiobased;
 
-    $('#nextstep').removeClass('error');
-    $('#Investment').removeClass("error");
+  $("#Investment-label").text("$" + Investment);
 
-    var resultratio = (ratio / ratiobased);
+  var Shares = (Investment / SharePrice).toFixed(4);
 
-    $('#Investment-label').text("$" + Investment);
+  $("#Shares-label").text(Shares + " Shares");
 
-    var Shares = (Investment / SharePrice).toFixed(4);
+  var PercentageofEarnings = (resultratio * Shares).toFixed(5);
 
-    $('#Shares-label').text(Shares + " Shares");
+  $("#PercentageofEarnings-label").text(PercentageofEarnings + "%");
 
+  var $payout = ($total * PercentageofEarnings).toFixed(4);
 
-    var PercentageofEarnings = (resultratio * Shares).toFixed(5);
+  var $payoutotal = parseFloat($payout - Investment);
 
-    $('#PercentageofEarnings-label').text(PercentageofEarnings + "%");
+  $("#Payout-label").text("$" + $payoutotal);
 
-    var $payout = ($total * PercentageofEarnings).toFixed(4);
+  //$('#slider a').html('<label>'+$Investment+'</label>');
+  $.fn.digits = function () {
+    return this.each(function () {
+      $(this).text(
+        $(this)
+          .text()
+          .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+      );
+    });
+  };
 
-    var $payoutotal = parseFloat($payout - Investment);
-
-    $('#Payout-label').text("$" + $payoutotal);
-
-
-
-    //$('#slider a').html('<label>'+$Investment+'</label>');
-    $.fn.digits = function() {
-        return this.each(function() {
-            $(this).text($(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-        })
-    }
-
-    $(".comma").digits();
-
+  $(".comma").digits();
 }
 
+$(function () {
+  $("input.number_only").keyup(function (event) {
+    // skip for arrow keys
+    if (event.which >= 37 && event.which <= 40) return;
 
-$(function() {
-
-    $('input.number_only').keyup(function(event) {
-
-        // skip for arrow keys
-        if (event.which >= 37 && event.which <= 40) return;
-
-        // format number
-        $(this).val(function(index, value) {
-            return value
-                .replace(/\D/g, "")
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        });
+    // format number
+    $(this).val(function (index, value) {
+      return value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     });
+  });
 });
