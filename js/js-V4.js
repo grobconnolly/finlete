5b846816a3ca6a9e60f69d2e0a7d8f739b4b82cc
shoaibzaheer95 <shoaibzaheer95@gmail.com>
2023-11-23 18:05:30 +0500

    script into file, set jquery perior

---
+++ js/js-V4.js
@@ -0,0 +1,72 @@
+ 
+// on scroll sticky nav
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll >= 150) {
+        //clearHeader, not clearheader - caps H
+        $(".top-bar").addClass("sticky");
+    }
+    else{
+        $(".top-bar").removeClass("sticky");
+    }
+}); //missing );
+
+    
+        
+// swiper for profile section
+var swiper = new Swiper(".swiper", {
+    speed: 400,
+    spaceBetween: 10,
+    initialSlide: 0,
+    //truewrapper adoptsheight of active slide
+    autoHeight: false,
+    // Optional parameters
+    direction: 'horizontal',
+    // delay between transitions in ms
+    //Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        },
+    // "slide", "fade", "cube", "coverflow" or "flip"
+    effect: 'slide',
+    // Distance between slides in px.
+    spaceBetween: 20,
+    //
+    slidesPerView: "auto",
+    //
+    centeredSlides: false,
+    //
+    slidesOffsetBefore: 0,
+    //
+    grabCursor: true,
+    
+
+})        
+
+// FAQ accord
+
+if ($('#MTaccordion').length) {
+
+    var $panels = $('.panel');
+
+    // add active class to first open panel
+    // only necessary if this panel is open by default on your accordion
+    //$panels.first().addClass('active');
+
+    $('.panel-heading a[data-toggle="collapse"]').on('click', function (e) {
+
+        $this = $(this);
+
+        $panel = $this.parents('.panel');
+
+        $panels.not( $panel ).removeClass('active');
+
+        $panel.toggleClass('active');
+
+    });
+}
+
