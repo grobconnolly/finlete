21b6dbe467f9774481a730a81044ffb45e5cbff8
shoaibzaheer95 <shoaibzaheer95@gmail.com>
2024-03-14 16:43:26 +0500

    added combine css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
 
 function combineAndMinifyCSS() {
-    return gulp.src(['css/home-v4.css', 'css/mobile.css']) // Paths to your CSS files
+    return gulp.src(['css/bootstrap.css','css/home-v4.css', 'css/mobile.css']) // Paths to your CSS files
         .pipe(concat('styles.min.css')) // Concatenate into one file
         .pipe(cleanCSS()) // Minify CSS
         .pipe(gulp.dest('css')); // Output directory
