3fc7c6f5a7b448abcc6d757bf976dba886954e04
shoaibzaheer95 <shoaibzaheer95@gmail.com>
2024-03-16 14:47:40 +0500

    addded news section, adjust calendly widget and make code position with cdns

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
+//const uglify = require('gulp-uglify');
 
 // Task to minify and concatenate JavaScript files
-gulp.task('minify-js', function() {
-    return gulp.src(['js/jquery-ui.js', 'js/bootstrap.js']) // Source files
-        .pipe(concat('global.min.js')) // Concatenate into one file
-        .pipe(uglify()) // Minify the files
-        .pipe(gulp.dest('js')); // Output directory for minified JS
-});
+// gulp.task('minify-js', function() {
+//     return gulp.src(['js/jquery-ui.js', 'js/bootstrap.js']) // Source files
+//         .pipe(concat('global.min.js')) // Concatenate into one file
+//         .pipe(uglify()) // Minify the files
+//         .pipe(gulp.dest('js')); // Output directory for minified JS
+// });
 function combineAndMinifyCSS() {
-    return gulp.src(['css/jquery-ui.css','css/bootstrap.css', 'css/home-v4.css', 'css/mobile.css']) // Paths to your CSS files
+    return gulp.src(['css/bootstrap.min.css', 'css/home-v4.css', 'css/mobile.css']) // Paths to your CSS files
         .pipe(concat('styles.min.css')) // Concatenate into one file
         .pipe(cleanCSS()) // Minify CSS
         .pipe(gulp.dest('css')); // Output directory for minified CSS
 }
 
-exports.default = gulp.series('minify-js', combineAndMinifyCSS);
+exports.default = gulp.series(combineAndMinifyCSS);
