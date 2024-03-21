155e37b95eafb5985870c3137140743b9e664493
shoaibzaheer95 <shoaibzaheer95@gmail.com>
2024-03-21 16:34:41 +0500

    swap nav scroll case

--- js/js-V4.js
+++ js/js-V4.js
@@ -2,26 +2,6 @@ $(window).scroll(function() {
     var e = $(window).scrollTop();
     150 <= e ? $(".top-bar").addClass("sticky") : $(".top-bar").removeClass("sticky"), 500 <= e ? $(".time-offer-echedry").addClass("stickft") : $(".time-offer-echedry").removeClass("stickft")
 });
-// Smooth scroll to section when clicking on a link with href="#id"
-document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a[href^="#"]');
-    links.forEach(function(link) {
-        link.addEventListener('click', function(e) {
-            e.preventDefault(); // Prevent default link behavior
-            
-            var targetId = this.getAttribute('href').substring(1); // Get target section id
-            var targetSection = document.getElementById(targetId); // Get target section element
-            if (targetSection) {
-                var offset = 60; // Offset for header height
-                var targetPosition = targetSection.offsetTop - offset; // Calculate target position
-                window.scrollTo({
-                    top: targetPosition,
-                    behavior: 'smooth' // Smooth scroll behavior
-                });
-            }
-        });
-    });
-});
 
 var $panels, swiper = new Swiper(".swiper", {
     speed: 400,
